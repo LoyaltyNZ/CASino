aff5a48d64bb64f8afb9b9d16d48e1abf7500674
Ben Greville <nojamin@live.com>
2018-09-21 12:43:27 +1200

    Updates factory_girl references to factory_bot

--- spec/support/factories/proxy_ticket_factory.rb
+++ spec/support/factories/proxy_ticket_factory.rb
@@ -1,4 +1,4 @@
-require 'factory_girl'
+require 'factory_bot'
 
 FactoryBot.define do
   factory :proxy_ticket, class: CASino::ProxyTicket do
--- spec/support/factories/two_factor_authenticator_factory.rb
+++ spec/support/factories/two_factor_authenticator_factory.rb
@@ -1,4 +1,4 @@
-require 'factory_girl'
+require 'factory_bot'
 require 'rotp'
 
 FactoryBot.define do
