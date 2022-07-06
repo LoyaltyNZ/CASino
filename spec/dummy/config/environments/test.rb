3ae3752c7da31bea95822b53af7b7519d14d17cc
David Oram <david.oram99@gmail.com>
2022-07-06 14:13:43 +1200

    DS-1946 Added CI/CD to build / test / release the gem

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  # Uncomment this to see unit test sql logs
+  # ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 end
